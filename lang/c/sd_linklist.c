15af0b8aa3fcb14d67c91b1efa5c9f29c23483ea
sunday <synchronized@yeah.net>
2019-03-07 20:33:15 +0800

    精简连表代码

--- lang/c/sd_linklist.c
+++ lang/c/sd_linklist.c
@@ -3,36 +3,48 @@
 #include <string.h>
 
 #define sd_llist_size 5
-typedef struct sd_llist {
+typedef struct sd_llist_node {
   char elt;
-  struct sd_llist *next;
+  struct sd_llist_node *next;
+} sd_llist_node_t;
+
+typedef struct sd_llist {
+  sd_llist_node_t *head;
 } sd_llist_t;
 
-sd_llist_t * sd_llist_init(int size);
-int sd_llist_display(sd_llist_t *t);
-int sd_llist_insert(sd_llist_t **t, int elt, int pos);
-int sd_llist_update(sd_llist_t **t, int elt, int pos);
-int sd_llist_delete(sd_llist_t **t, int pos);
+int sd_llist_init(sd_llist_t *ll, int size);
+int sd_llist_display(sd_llist_t *ll);
+int sd_llist_insert(sd_llist_t *ll, int elt, int pos);
+int sd_llist_update(sd_llist_t *ll, int elt, int pos);
+int sd_llist_delete(sd_llist_t *ll, int pos);
 
 
-sd_llist_t * sd_llist_init(int size) {
-  printf("sd_llist_init(%d)\n", size);
-  sd_llist_t *head = NULL;
-  sd_llist_t **next = &head;
-  sd_llist_t *temp = NULL;
+int sd_llist_init(sd_llist_t *ll, int size) {
+  if (!ll) {
+    return -1;
+  }
+  memset(ll, 0, sizeof(sd_llist_t));
+  sd_llist_node_t **next = &ll->head;
+  sd_llist_node_t *temp = NULL;
 
   for (int i=0; i<size; i++) {
-    temp = (sd_llist_t*)malloc(sizeof(sd_llist_t));
-    memset(temp, 0, sizeof(sd_llist_t));
+    temp = (sd_llist_node_t*)malloc(sizeof(sd_llist_node_t));
+    if (!temp) {
+      return -2;
+    }
+    memset(temp, 0, sizeof(sd_llist_node_t));
     temp->elt = i;
     *next = temp;
     next = &(*next)->next;
   }
-  return head;
+  return 0;
 }
 
-int sd_llist_display(sd_llist_t *t) {
-  sd_llist_t *next = t;
+int sd_llist_display(sd_llist_t *ll) {
+  if (!ll) {
+    return -1;
+  }
+  sd_llist_node_t *next = ll->head;
   while(next) {
     printf("{elt: %d} -> ", next->elt) ;
     next = next->next;
@@ -41,111 +53,140 @@ int sd_llist_display(sd_llist_t *t) {
   return 0;
 }
 
-int sd_llist_insert(sd_llist_t **t, int elt, int pos) {
+int sd_llist_insert(sd_llist_t *ll, int elt, int pos) {
+  if (!ll) {
+    return -1;
+  }
   int i = 0;
-  sd_llist_t **node = t;
-  sd_llist_t *new_node = NULL;
-  for (i=0; i<pos && *node; i++, node=&(*node)->next) {
+  sd_llist_node_t **next = &ll->head;
+  for (; i<pos && *next; i++, next = &(*next)->next) {
     ;
   }
   if (i != pos) {
-    return -1; //t invalid
+    return -2; //t invalid
   }
 
-  new_node = (sd_llist_t*)malloc(sizeof(sd_llist_t));
-  if (!new_node) {
+  sd_llist_node_t *node = (sd_llist_node_t*)malloc(sizeof(sd_llist_node_t));
+  if (!node) {
     return -3; //new node invalid
   }
-  memset(new_node, 0, sizeof(sd_llist_t));
-  new_node->next = (*node);
-  new_node->elt = elt;
-  *node = new_node;
+  memset(node, 0, sizeof(sd_llist_t));
+  node->elt = elt;
+  node->next = *next;
+  *next = node;
   return 0;
 }
 
-int sd_llist_update(sd_llist_t **t, int elt, int pos) {
+int sd_llist_update(sd_llist_t *ll, int elt, int pos) {
+  if (!ll) {
+    return -1;
+  }
   int i = 0;
-  sd_llist_t **node = t;
-  for (i=0; i<pos && *node; i++, node=&(*node)->next) {
+  sd_llist_node_t *curr = ll->head;
+  while (i<pos && curr) {
+    curr = curr->next;
+    i++;
+  }
+  if (i != pos) {
+    return -2; //pos invalid
+  }
+
+  if (!curr) {
+    return -3; //pos invalid
+  }
+
+  curr->elt = elt;
+  return 0;
+}
+
+int sd_llist_delete(sd_llist_t *ll, int pos) {
+  if (!ll) {
+    return -1;
+  }
+  int i = 0;
+  sd_llist_node_t **next = &ll->head;
+  for (; i<pos && *next; i++, next=&(*next)->next) {
     ;
   }
   if (i != pos) {
     return -1; //pos invalid
   }
-  if (!*node) {
-    return -2; //pos invalid
-  }
 
-  (*node)->elt = elt;
+  if (*next) {
+    *next = (*next)->next;
+  }
   return 0;
 }
 
-int sd_llist_delete(sd_llist_t **t, int pos) {
-  int i = 0;
-  sd_llist_t **node = t;
-  for (i=0; i<pos && *node; i++, node=&(*node)->next) {
-    ;
-  }
-  if (i != pos) {
-    return -1; //pos invalid
-  }
-  if (!*node) {
-    return -2; //pos invalid
-  }
-
-  *node = (*node)->next;
-  return 0;
-}
-
-void sd_llist_test_insert(sd_llist_t **t, int elt, int pos) {
-  int code = sd_llist_insert(t, elt, pos);
-  printf("sd_llist_insert(t, %d, %d) = %d\n", elt, pos, code);
+int sd_llist_test_init(sd_llist_t *ll, int size) {
+  int code = 0;
+  code = sd_llist_init(ll, size);
+  printf("sd_llist_init(t, %d) = %d\n", size, code);
   if (code != 0) {
     printf("    Error\n");
+  } else {
+    printf("    ");
+    sd_llist_display(ll);
   }
-  printf("    after insert: ");
-  sd_llist_display(*t);
+  return code;
 }
 
-void sd_llist_test_update(sd_llist_t **t, int elt, int pos) {
-  int code = sd_llist_update(t, elt, pos);
-  printf("sd_llist_update(t, %d, %d) = %d\n", elt, pos, code);
+void sd_llist_test_insert(sd_llist_t *ll, int elt, int pos) {
+  int code = sd_llist_insert(ll, elt, pos);
+  printf("sd_llist_insert(ll, %d, %d) = %d\n", elt, pos, code);
   if (code != 0) {
     printf("    Error\n");
+  } else {
+    printf("    ");
+    sd_llist_display(ll);
   }
-  printf("    after update: ");
-  sd_llist_display(*t);
 }
 
-void sd_llist_test_delete(sd_llist_t **t, int pos) {
-  int code = sd_llist_delete(t, pos);
-  printf("sd_llist_delete(t, %d) = %d\n", pos, code);
+void sd_llist_test_update(sd_llist_t *ll, int elt, int pos) {
+  int code = sd_llist_update(ll, elt, pos);
+  printf("sd_llist_update(ll, %d, %d) = %d\n", elt, pos, code);
   if (code != 0) {
     printf("    Error\n");
+  } else {
+    printf("    ");
+    sd_llist_display(ll);
   }
-  printf("    after delete: ");
-  sd_llist_display(*t);
+}
+
+void sd_llist_test_delete(sd_llist_t *ll, int pos) {
+  int code = sd_llist_delete(ll, pos);
+  printf("sd_llist_delete(ll, %d) = %d\n", pos, code);
+  if (code != 0) {
+    printf("    Error\n");
+  } else {
+    printf("    ");
+    sd_llist_display(ll);
+  }
 }
 
 int main() {
   printf("linklist:\n");
   int code = 0;
-  sd_llist_t *t = sd_llist_init(sd_llist_size);
-  printf("    init: ");
-  sd_llist_display(t);
-  sd_llist_test_insert(&t, 10, 0);
-  sd_llist_test_insert(&t, 11, 2);
-  sd_llist_test_insert(&t, 12, 7);
+  sd_llist_t ll;
+  code = sd_llist_test_init(&ll, sd_llist_size);
+  if (code) {
+    return -1;
+  }
+  sd_llist_test_insert(&ll, 10, 0);
+  sd_llist_test_insert(&ll, 11, 2);
+  sd_llist_test_insert(&ll, 12, 7);
+  sd_llist_test_insert(&ll, 13, 9);
 
   printf("\n");
-  sd_llist_test_update(&t, -1, 0);
-  sd_llist_test_update(&t, -1, 2);
-  sd_llist_test_update(&t, -1, 7);
-  sd_llist_test_update(&t, -1, 8);
+  sd_llist_test_update(&ll, -1, 0);
+  sd_llist_test_update(&ll, -1, 2);
+  sd_llist_test_update(&ll, -1, 7);
+  sd_llist_test_update(&ll, -1, 8);
 
   printf("\n");
-  sd_llist_test_delete(&t, 7);
-  sd_llist_test_delete(&t, 2);
-  sd_llist_test_delete(&t, 0);
-  sd_llist_test_delete(&t, 8);
+  sd_llist_test_delete(&ll, 7);
+  sd_llist_test_delete(&ll, 2);
+  sd_llist_test_delete(&ll, 0);
+  sd_llist_test_delete(&ll, 8);
+  return 0;
 }
