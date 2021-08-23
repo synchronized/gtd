2c9b5452e82cfccd042e18f3104d90f27c13e45c
sunday <synchronized@yeah.net>
2021-08-23 09:44:37 +0800

    增加leecode刷题记录-零移动

---
+++ lang/c-cpp/leecode/moveZeroes.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  void moveZeroes(vector<int>& nums) {
+    vector<int>::size_type i = 0, j = 0;
+    for (i=0; i<nums.size(); i++) {
+      if (nums[i] == 0) {
+        continue;
+      }
+      if (i != j) {
+        nums[j] = nums[i];
+      }
+      j++;
+    }
+    for (; j<nums.size(); j++) {
+      nums[j] = 0;
+    }
+  }
+};
+
+std::string vectorToString(vector<int> nums) {
+  std::string buf = "{";
+  for (auto it = nums.begin(); it!= nums.end(); it++) {
+    if (it != nums.begin()) {
+      buf += ", ";
+    }
+    buf += std::to_string(*it);
+  }
+  buf += "}";
+  return buf;
+}
+
+int main() {
+  Solution solution;
+  vector<vector<int>> data {
+                          {1,2,3,4},
+                          {0,1,2,3,4},
+                          {0,1,0,2,3,4},
+  };
+  for (auto it = data.begin(); it != data.end(); it++) {
+    vector<int> nums = *it;
+    cout << "solution.moveZeroes(" << vectorToString(nums) << "):";
+    solution.moveZeroes(nums);
+    cout << vectorToString(nums) << endl;
+  }
+  return 0;
+}
