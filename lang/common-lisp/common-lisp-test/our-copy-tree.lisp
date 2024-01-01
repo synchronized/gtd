11cef91c8823b409076ac30585c29523075f2c58
sunday <synchronized@yeah.net>
2024-01-01 09:35:51 +0800

    删除多余的例程代码

--- lang/common-lisp/common-lisp-test/our-copy-tree.lisp
+++
@@ -1,6 +0,0 @@
-
-(defun our-copy-tree (tr)
-  (if (atom tr)
-      tr
-      (cons (our-copy-tree (car tr))
-            (our-copy-tree (cdr tr)))))
--- lang/lisp/ANSI-Common-Lisp/2.lisp
+++
@@ -1,4 +0,0 @@
-(defun test2-1 ()
-  (print '(a b c))
-  (list 'a 'b 'c)
-  )
